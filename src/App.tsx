b7fe3eba26013258366a14bb01d8c88578869188
igonak <igor.gonak@hmmh.de>
2022-05-10 10:57:16 +0200

    Add controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Stack, Button } from "react-bootstrap";
+import {
+  Play,
+  StopCircle,
+  SkipBackward,
+  SkipForwardCircle,
+  PauseCircle,
+  Heart,
+  HeartFill,
+  Shuffle,
+  ArrowRepeat,
+} from "react-bootstrap-icons";
+
+//
+///Play, playplay-btn, play-circle, playfill
 
 function App() {
-  return <div></div>;
+  return (
+    <Card>
+      <p>Interpreter: </p>
+      <p>Titel: </p>
+      <Stack direction="horizontal" gap={3}>
+        {/* <Button size="sm" className="rounded-circle"> */}
+        <SkipBackward />
+        {/* </Button> */}
+        <Play />
+        <PauseCircle />
+        <StopCircle />
+        <SkipForwardCircle />
+        <Shuffle />
+        <ArrowRepeat />
+        <Heart />
+      </Stack>
+    </Card>
+  );
 }
 
 export default App;
